ce641e2237e6fec8d00861c08137c5e4c1414a3c
MohammadZubair18 <116421874+MohammadZubair18@users.noreply.github.com>
2022-10-31 22:17:38 +0530

    Update Codefile.py

--- Codefile.py
+++ Codefile.py
@@ -1,22 +1,54 @@
-def add(n1,n2):
-  return n1+n2
-def subtract(n1,n2):
-  return n1-n2
-def multiplication(n1,n2):
-  return n1*n2
-def division(n1,n2):
-  return(n1/n2)
-dict={
-"+":add,
-"-":subtract,
-"*":multiplication,
-"/":division,
+#printing logo
+print(
+"""
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+
+""")
+def multiply(n1, n2):
+  return n1 * n2
+
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
 }
-num1=int(input("Enter number first number: "))
-num2=int(input("Enter number second number: "))
-for operators in dict:
-  print(operators)
-operation_symbol=input("pick any one of the operation from above")
-calucation_function=dict[operation_symbol]
-answer = calucation_function(num1,num2)
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+def calculator():
+
+  num1 = float(input("What's the first number?: "))
+  for symbol in operations:
+    print(symbol)
+  should_continue = True
+ 
+  while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+      num1 = answer
+    else:
+      should_continue = False
+      clear()
+      calculator()
+
+calculator()
